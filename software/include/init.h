b332622bea32327a68a75da09ef07cbff302c9bc
lchagnoleau <loic.chagnoleau@sigfox.com>
2018-08-24 11:20:23 +0200

    add hardwer project

---
+++ software/include/init.h
@@ -0,0 +1,14 @@
+/*
+ * init.h
+ *
+ *  Created on: 12 avr. 2018
+ *      Author: lchagnoleau
+ */
+
+#ifndef INIT_H_
+#define INIT_H_
+
+void init(void);
+void SystemClock_Config(void);
+
+#endif /* INIT_H_ */
---
+++ software/include/main.h
@@ -0,0 +1,17 @@
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f0xx_hal.h"
+#include "init.h"
+#include "usb.h"
+
+/* Private define ------------------------------------------------------------*/
+
+#define LED2_Pin GPIO_PIN_3
+#define LED2_GPIO_Port GPIOA
+#define LED1_Pin GPIO_PIN_4
+#define LED1_GPIO_Port GPIOA
+
+#endif /* __MAIN_H__ */
