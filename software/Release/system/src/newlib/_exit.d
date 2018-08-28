cac1c988da1d12d1d2155df032fd7486a9e9f66b
cbeauduffe <cedric.beauduffe@sigfox.com>
2018-08-28 23:31:17 +0200

    Remove Files

--- software/Release/system/src/newlib/_exit.d
+++
@@ -1,4 +0,0 @@
-system/src/newlib/_exit.o: ../system/src/newlib/_exit.c \
- /home/cbeauduffe/git/vigilant-winner/software/system/include/diag/Trace.h
-
-/home/cbeauduffe/git/vigilant-winner/software/system/include/diag/Trace.h:
--- software/Release/system/src/newlib/assert.d
+++
@@ -1,4 +0,0 @@
-system/src/newlib/assert.o: ../system/src/newlib/assert.c \
- /home/cbeauduffe/git/vigilant-winner/software/system/include/diag/Trace.h
-
-/home/cbeauduffe/git/vigilant-winner/software/system/include/diag/Trace.h:
