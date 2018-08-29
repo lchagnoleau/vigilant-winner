333785a66a1bab3d05845c8f13b0d1812eecd7c6
lchagnoleau <loic.chagnoleau@sigfox.com>
2018-08-29 17:46:26 +0200

    windows works

--- software/src/usb.c
+++ software/src/usb.c
@@ -9,13 +9,6 @@
 #include "stm32f0xx_hal.h"
 #include "usb_device.h"
 
-// HID Keyboard
-struct keyboardHID_t {
-    uint8_t id;
-    uint8_t modifiers;
-    uint8_t key;
-};
-
 void USB_init(void)
 {
   MX_USB_DEVICE_Init();
@@ -23,49 +16,15 @@ void USB_init(void)
 
 void USB_send_char(unsigned char c, unsigned char mod)
 {
-//  struct keyboardHID_t keyboardHID;
-//  keyboardHID.id = 1;
-//  keyboardHID.modifiers = 0;
-//  keyboardHID.key = 0;
-//
-//  keyboardHID.key = c;
-//  keyboardHID.modifiers = mod;
-//  USBD_HID_SendReport(&hUsbDeviceFS, &keyboardHID, sizeof(struct keyboardHID_t));
-//  HAL_Delay(10);
-//  keyboardHID.modifiers = 0;
-//  keyboardHID.key = 0;
-//  USBD_HID_SendReport(&hUsbDeviceFS, &keyboardHID, sizeof(struct keyboardHID_t));
-//  HAL_Delay(10);
+    uint8_t press_report[8] = {0x00};
+    press_report[0] = mod;
+    press_report[2] = c;
 
-  uint8_t press_report[8] = {0x00};
-      press_report[2] = 7;  // send 'd' 
-      press_report[3] = 18;  // send '0' 
-      press_report[4] = 16;  // send 'm' 
-      press_report[5] = 8;  // send 'e' 
-      press_report[6] = 17;  // send 'n' 
-      
-      USBD_HID_SendReport(&hUsbDeviceFS, press_report, 8);
-      
-      HAL_Delay(50); 
-      
-      //press_report[2] = 0;  // send button release
-      press_report[2] = 0;
-      press_report[3] = 0;
-      press_report[4] = 0;
-      press_report[5] = 0;
-      press_report[6] = 0;
-      
-      USBD_HID_SendReport(&hUsbDeviceFS, press_report, 8);
+    USBD_HID_SendReport(&hUsbDeviceFS, press_report, 8);
+    HAL_Delay(30); 
+    press_report[0] = 0;
+    press_report[2] = 0;
 
-  // uint8_t press_report[4] = {0x00};
-  // press_report[0] = 0x01;
-  // press_report[1] = mod;
-  // press_report[3] = c;
-  // USBD_HID_SendReport(&hUsbDeviceFS, press_report, 4);
-  // HAL_Delay(10);
-  // press_report[0] = 0x01;
-  // press_report[1] = 0;
-  // press_report[3] = 0;
-  // USBD_HID_SendReport(&hUsbDeviceFS, press_report, 4);
-  // HAL_Delay(10);
+    USBD_HID_SendReport(&hUsbDeviceFS, press_report, 8);
+    HAL_Delay(30); 
 }
